1e52cbadbe830e7a07b3bbdd15313a3552ad9550
anisul islam <anisulislam980340@gmail.com>
2023-05-27 22:48:02 +0600

    string basic

---
+++ string.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s="anis";
+    s="anis muslim";
+    string s1="afrin";
+    string s2="afrin";
+    if(s1==s2){
+        cout<<"same"<<endl;
+    }
+    else{
+        cout<<"not same"<<endl;
+    }
+    cout<<s<<endl;
+    return 0;
+}
